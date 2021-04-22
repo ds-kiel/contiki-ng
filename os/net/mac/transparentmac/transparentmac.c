9c7a253bcbf6773b3eb41ed2553036d10068eb0a
Oliver Harms <oha@informatik.uni-kiel.de>
2021-04-22 15:12:42 +0200

    Transparent MAC Layer added

---
+++ os/net/mac/transparentmac/transparentmac.c
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2010, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A MAC protocol that behaves as a transparent layer.
+ * \author
+ *         Laura Harms <oha@informatik.uni-kiel.de>
+ */
+
+#include "net/mac/transparentmac/transparentmac.h"
+// #include "net/mac/csma/csma-output.h"
+#include "net/mac/mac-sequence.h"
+#include "net/packetbuf.h"
+#include "net/netstack.h"
+#include "lib/list.h"
+// #include "lib/memb.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "TMAC"
+#define LOG_LEVEL LOG_LEVEL_MAC
+
+
+// LIST(neighbor_list);
+
+
+// static void
+// init_sec(void)
+// {
+// #if LLSEC802154_USES_AUX_HEADER
+//   if(packetbuf_attr(PACKETBUF_ATTR_SECURITY_LEVEL) ==
+//      PACKETBUF_ATTR_SECURITY_LEVEL_DEFAULT) {
+//     packetbuf_set_attr(PACKETBUF_ATTR_SECURITY_LEVEL,
+//                        CSMA_LLSEC_SECURITY_LEVEL);
+//   }
+// #endif
+// }
+/*---------------------------------------------------------------------------*/
+static void
+send_packet(mac_callback_t sent, void *ptr)
+{
+
+  // init_sec();
+
+  // csma_output_packet(sent, ptr);
+
+
+  static uint8_t initialized = 0;
+  static uint8_t seqno;
+  int hdr_len;
+  int ret;
+  int last_sent_ok = 0;
+
+  if(!initialized) {
+    initialized = 1;
+    /* Initialize the sequence number to a random value as per 802.15.4. */
+    seqno = random_rand();
+  }
+
+  if(seqno == 0) {
+    /* PACKETBUF_ATTR_MAC_SEQNO cannot be zero, due to a pecuilarity
+       in framer-802154.c. */
+    seqno++;
+  }
+  packetbuf_set_attr(PACKETBUF_ATTR_MAC_SEQNO, seqno++);
+  packetbuf_set_attr(PACKETBUF_ATTR_FRAME_TYPE, FRAME802154_DATAFRAME);
+
+  packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_node_addr);
+  packetbuf_set_attr(PACKETBUF_ATTR_MAC_ACK, 1);
+
+  // SECURITY FRAME?
+  hdr_len = NETSTACK_FRAMER.create();
+  if(hdr_len < 0) {
+    LOG_ERR("failed to create packet, seqno: %d\n", packetbuf_attr(PACKETBUF_ATTR_MAC_SEQNO));
+    ret = MAC_TX_ERR_FATAL;
+  } else {
+    int is_broadcast;
+    uint8_t dsn;
+    dsn = ((uint8_t *)packetbuf_hdrptr())[2] & 0xff;
+
+    NETSTACK_RADIO.prepare(packetbuf_hdrptr(), packetbuf_totlen());
+
+    is_broadcast = packetbuf_holds_broadcast();
+
+    if(NETSTACK_RADIO.receiving_packet() ||
+       (!is_broadcast && NETSTACK_RADIO.pending_packet())) {
+
+      /* Currently receiving a packet over air or the radio has
+         already received a packet that needs to be read before
+         sending with auto ack. */
+      ret = MAC_TX_COLLISION;
+    } else {
+
+      switch(NETSTACK_RADIO.transmit(packetbuf_totlen())) {
+      case RADIO_TX_OK:
+        if(is_broadcast) {
+          ret = MAC_TX_OK;
+        } else {
+          /* Check for ack */
+
+          /* Wait for max TMAC_ACK_WAIT_TIME */
+          RTIMER_BUSYWAIT_UNTIL(NETSTACK_RADIO.pending_packet(), TMAC_ACK_WAIT_TIME);
+
+          ret = MAC_TX_NOACK;
+          if(NETSTACK_RADIO.receiving_packet() ||
+             NETSTACK_RADIO.pending_packet() ||
+             NETSTACK_RADIO.channel_clear() == 0) {
+            int len;
+            uint8_t ackbuf[TMAC_ACK_LEN];
+
+            /* Wait an additional TMAC_AFTER_ACK_DETECTED_WAIT_TIME to complete reception */
+            RTIMER_BUSYWAIT_UNTIL(NETSTACK_RADIO.pending_packet(), TMAC_AFTER_ACK_DETECTED_WAIT_TIME);
+
+            if(NETSTACK_RADIO.pending_packet()) {
+              len = NETSTACK_RADIO.read(ackbuf, TMAC_ACK_LEN);
+              if(len == TMAC_ACK_LEN && ackbuf[2] == dsn) {
+                /* Ack received */
+                ret = MAC_TX_OK;
+              } else {
+                /* Not an ack or ack not for us: collision */
+                ret = MAC_TX_COLLISION;
+              }
+            }
+          }
+        }
+        break;
+      case RADIO_TX_COLLISION:
+        ret = MAC_TX_COLLISION;
+        break;
+      default:
+        ret = MAC_TX_ERR;
+        break;
+      }
+    }
+  }
+  mac_call_sent_callback(sent, ptr, ret, 1);
+}
+/*---------------------------------------------------------------------------*/
+static void
+input_packet(void)
+{
+#if TMAC_SEND_SOFT_ACK
+  uint8_t ackdata[TMAC_ACK_LEN];
+#endif
+
+  int hdr_len;
+
+  hdr_len = NETSTACK_FRAMER.parse();
+  if(hdr_len < 0) {
+    LOG_ERR("failed to parse %u\n", packetbuf_datalen());
+  }
+
+  if(packetbuf_datalen() == TMAC_ACK_LEN && ((uint8_t *)packetbuf_dataptr())[0] == FRAME802154_ACKFRAME) {
+    /* Ignore ack packets */
+    LOG_DBG("ignored ack\n");
+  } else if(!linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),
+                                         &linkaddr_node_addr) &&
+            !packetbuf_holds_broadcast()) {
+    LOG_WARN("not for us\n");
+  } else if(linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_SENDER), &linkaddr_node_addr)) {
+    LOG_WARN("frame from ourselves\n");
+  } else {
+    int duplicate = 0;
+
+    /* Check for duplicate packet. */
+    duplicate = mac_sequence_is_duplicate();
+    if(duplicate) {
+      /* Drop the packet. */
+      LOG_WARN("drop duplicate link layer packet from ");
+      LOG_WARN_LLADDR(packetbuf_addr(PACKETBUF_ADDR_SENDER));
+      LOG_WARN_(", seqno %u\n", packetbuf_attr(PACKETBUF_ATTR_MAC_SEQNO));
+    } else {
+      mac_sequence_register_seqno();
+    }
+
+#if TMAC_SEND_SOFT_ACK
+    if(packetbuf_attr(PACKETBUF_ATTR_MAC_ACK)) {
+      ackdata[0] = FRAME802154_ACKFRAME;
+      ackdata[1] = 0;
+      ackdata[2] = ((uint8_t *)packetbuf_hdrptr())[2];
+      NETSTACK_RADIO.send(ackdata, TMAC_ACK_LEN);
+    }
+#endif /* TMAC_SEND_SOFT_ACK */
+    if(!duplicate) {
+      LOG_INFO("received packet from ");
+      LOG_INFO_LLADDR(packetbuf_addr(PACKETBUF_ADDR_SENDER));
+      LOG_INFO_(", seqno %u, len %u\n", packetbuf_attr(PACKETBUF_ATTR_MAC_SEQNO), packetbuf_datalen());
+      NETSTACK_NETWORK.input();
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+static int
+on(void)
+{
+  return NETSTACK_RADIO.on();
+}
+/*---------------------------------------------------------------------------*/
+static int
+off(void)
+{
+  return NETSTACK_RADIO.off();
+}
+/*---------------------------------------------------------------------------*/
+static void
+init(void)
+{
+  radio_value_t radio_max_payload_len;
+
+  /* Check that the radio can correctly report its max supported payload */
+  if(NETSTACK_RADIO.get_value(RADIO_CONST_MAX_PAYLOAD_LEN, &radio_max_payload_len) != RADIO_RESULT_OK) {
+    LOG_ERR("! radio does not support getting RADIO_CONST_MAX_PAYLOAD_LEN. Abort init.\n");
+    return;
+  }
+
+// #if LLSEC802154_USES_AUX_HEADER
+// #ifdef CSMA_LLSEC_DEFAULT_KEY0
+//   uint8_t key[16] = CSMA_LLSEC_DEFAULT_KEY0;
+//   csma_security_set_key(0, key);
+// #endif
+// #endif /* LLSEC802154_USES_AUX_HEADER */
+
+  // memb_init(&packet_memb);
+  // memb_init(&metadata_memb);
+  // memb_init(&neighbor_memb);
+
+  // csma_output_init();
+  on();
+}
+/*---------------------------------------------------------------------------*/
+static int
+max_payload(void)
+{
+  int framer_hdrlen;
+  radio_value_t max_radio_payload_len;
+  radio_result_t res;
+
+  // init_sec();
+
+  framer_hdrlen = NETSTACK_FRAMER.length();
+
+  res = NETSTACK_RADIO.get_value(RADIO_CONST_MAX_PAYLOAD_LEN,
+                                 &max_radio_payload_len);
+
+  if(res == RADIO_RESULT_NOT_SUPPORTED) {
+    LOG_ERR("Failed to retrieve max radio driver payload length\n");
+    return 0;
+  }
+
+  if(framer_hdrlen < 0) {
+    /* Framing failed, we assume the maximum header length */
+    framer_hdrlen = TMAC_MAC_MAX_HEADER;
+  }
+
+  return MIN(max_radio_payload_len, PACKETBUF_SIZE)
+    - framer_hdrlen;
+    // - LLSEC802154_PACKETBUF_MIC_LEN();
+}
+/*---------------------------------------------------------------------------*/
+const struct mac_driver transparentmac_driver = {
+  "TMAC",
+  init,
+  send_packet,
+  input_packet,
+  on,
+  off,
+  max_payload,
+};
+/*---------------------------------------------------------------------------*/
---
+++ os/net/mac/transparentmac/transparentmac.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A MAC protocol that behaves as a transparent layer.
+ * \author
+ *         Laura Harms <oha@informatik.uni-kiel.de>
+ */
+
+#ifndef TMAC_H_
+#define TMAC_H_
+
+#include "contiki.h"
+#include "net/mac/mac.h"
+#include "net/packetbuf.h"
+#include "net/netstack.h"
+#include "dev/radio.h"
+
+#ifdef TMAC_CONF_SEND_SOFT_ACK
+#define TMAC_SEND_SOFT_ACK TMAC_CONF_SEND_SOFT_ACK
+#else /* TMAC_CONF_SEND_SOFT_ACK */
+#define TMAC_SEND_SOFT_ACK 0
+#endif /* TMAC_CONF_SEND_SOFT_ACK */
+
+#ifdef TMAC_CONF_ACK_WAIT_TIME
+#define TMAC_ACK_WAIT_TIME TMAC_CONF_ACK_WAIT_TIME
+#else /* CSMA_CONF_ACK_WAIT_TIME */
+#define TMAC_ACK_WAIT_TIME                      RTIMER_SECOND / 2500
+#endif /* CSMA_CONF_ACK_WAIT_TIME */
+
+#ifdef TMAC_CONF_AFTER_ACK_DETECTED_WAIT_TIME
+#define TMAC_AFTER_ACK_DETECTED_WAIT_TIME TMAC_CONF_AFTER_ACK_DETECTED_WAIT_TIME
+#else /* TMAC_CONF_AFTER_ACK_DETECTED_WAIT_TIME */
+#define TMAC_AFTER_ACK_DETECTED_WAIT_TIME       RTIMER_SECOND / 1500
+#endif /* TMAC_CONF_AFTER_ACK_DETECTED_WAIT_TIME */
+
+#define TMAC_ACK_LEN 3
+
+/* just a default - with LLSEC, etc */
+#define TMAC_MAC_MAX_HEADER 21 //TODOLIV
+
+
+extern const struct mac_driver transparentmac_driver;
+
+/* CSMA security framer functions */
+// int csma_security_create_frame(void);
+// int csma_security_parse_frame(void);
+
+// /* key management for CSMA */
+// int csma_security_set_key(uint8_t index, const uint8_t *key);
+
+
+#endif /* TMAC_H_ */
+/**
+ * @}
+ * @}
+ */
